3bf30dbc629a2c61bec5d4993df8a8744f9e2a78
gustavosrosa <g.dsantosrosa@gmail.com>
2025-09-10 17:37:09 -0300

    TDLST: FEAT: Formulario

--- frontend/src/views/FormView.vue
+++ frontend/src/views/FormView.vue
@@ -2,15 +2,26 @@
     <div>
         <BForm v-if="show" @submit="onSubmit" @reset="onReset">
             <BFormGroup id="input-group-1" label="Tarefa:" label-for="input-1" class="mb-2">
-                <BFormInput id="input-1" v-model="form.email" type="text" placeholder="Digite a tarefa" required />
+                <BFormInput id="input-1" v-model="form.name" type="text" placeholder="Digite a tarefa" required />
             </BFormGroup>
 
             <BFormGroup id="input-group-2" label="Descrição da tarefa:" label-for="input-2" class="mb-4">
-                <BFormInput id="input-2" v-model="form.name" placeholder="Digite a descrição da tarefa (não obrigatória)" />
+                <BFormInput id="input-2" v-model="form.description"
+                    placeholder="Digite a descrição da tarefa (não obrigatória)" />
             </BFormGroup>
 
             <BButton type="submit" variant="primary" class="mr-2">Cadastrar tarefa</BButton>
             <BButton type="reset" variant="danger" :disabled="!form.email">Limpar</BButton>
+
+            <BModal v-model="modal" :title="'Ocorreu um erro'">
+                {{ errorMessage }}
+
+                <template #footer>
+                    <BButton variant="danger" @click="irParaLista">
+                        Voltar
+                    </BButton>
+                </template>
+            </BModal>
         </BForm>
     </div>
 
@@ -18,28 +29,43 @@
 
 <script setup>
 import { reactive, ref } from 'vue'
+import axios from 'axios';
+import router from '@/router';
+
+const modal = ref(false)
+const message = ref("");
 
 const form = reactive({
-    email: '',
     name: '',
-    food: null,
-    checked: [],
+    description: '',
 })
 const show = ref(true)
 
 const onSubmit = (event) => {
     event.preventDefault()
-    // eslint-disable-next-line no-alert
-    alert(JSON.stringify(form))
+
+    modal.value = true;
+
+    axios.post("https://todolist-api-vue-js-node.onrender.com/task", form).then(response => {
+        message.value = response.data;
+        console.log('User created:', response.data);
+    }).catch(error => {
+        message.value = error;
+        console.error('Error creating user:', error);
+    });
+
+
 }
+
+function irParaLista() {
+    router.push(['/']);
+}
+
 
 const onReset = (event) => {
     event.preventDefault()
-    // Reset our form values
-    form.email = ''
-    form.name = ''
-    form.food = null
-    form.checked = []
-   
+    form.name = '';
+    form.description = '';
+
 }
 </script>
