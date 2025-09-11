4c2c356a0b591247710a95ab1808e00c7eeba6a7
gustavosrosa <g.dsantosrosa@gmail.com>
2025-09-11 12:32:59 -0300

    TDLST: FEAT: Funcionalidade exclusao

--- frontend/src/components/TaskInfoComponent.vue
+++ frontend/src/components/TaskInfoComponent.vue
@@ -2,9 +2,11 @@
     <div>
         <BCard :title="props.task.name">
             <BCardText>{{ props.task.description }}</BCardText>
-            <BButton variant="primary" class="mr-2" @click="editar(props.task)">Editar</BButton>
-            <BButton href="#" variant="danger">Excluir</BButton>
+            <BButton variant="primary" class="mr-2" @click="update(props.task)">Editar</BButton>
+            <BButton href="#" variant="danger" @click="deleteTask(props.task.id)">Excluir</BButton>
         </BCard>
+        <ModalComponent :title="'OK'" :option="'RELOAD'" :message="message" :action="'Voltar'"
+                :modal="showModal" />
     </div>
 </template>
 
@@ -12,13 +14,28 @@
 
 import router from '@/router';
 import { useTaskStore } from '@/stores/taskStore';
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue';
+import axios from 'axios';
+import ModalComponent from './ModalComponent.vue';
 
 const props = defineProps(['task']);
+const message = ref("Retorno");
+const showModal = ref(false);
 
-function editar(task) {
+function update(task) {
     useTaskStore().currentTask = task;
     useTaskStore().origin = "EDIT";
     router.push({ name: 'form' })
 }
+
+function deleteTask(id) {
+    axios.delete(`https://todolist-api-vue-js-node.onrender.com/task/${id}`).then(response => {
+        showModal.value = true;
+        message.value = response.data;
+    }).catch(error => {
+        showModal.value = true;
+        message.value = error.response.data;
+    });
+}
+
 </script>
